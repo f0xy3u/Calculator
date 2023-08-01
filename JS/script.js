d0cbaa0e99a6de754d73274740031c36d4d0c18a
f0xy3u <123635828+f0xy3u@users.noreply.github.com>
2023-08-02 00:32:18 +0200

    getting ready for keyboard acces

--- JS/script.js
+++ JS/script.js
@@ -104,23 +104,234 @@ function displayUpdater(bottomText, equalButton, acButton) {
 }
 
 
-//function to make it easy to moderate decimal dot
+//function to make it easy acces all of buttons
 function decimalAdder() {
     if (operator == ``) {
         num1 += `.`
         displayUpdater(num1)
     } else if (operator != ``) {
-        num2 += `.`
-        displayUpdater(num2)
+        if (num2 == ``) {
+            num2 += `0.`
+            displayUpdater(num2)
+        } else {
+            num2 += `.`
+            displayUpdater(num2)
+        }
 
     } else {console.log(`Error occurred after button was clicked!`)}
 
     decimalButton.removeEventListener("click", decimalAdder)
 }
 
+function one() {
+    if (operator == ``) {
+        if(num1 == `0`) {
+            num1 = `1`
+            displayUpdater(num1)
+        } else {
+            num1 += `1`
+            displayUpdater(num1)
+        }
+    } else if (operator != ``) {
+        num2 += `1`
+        displayUpdater(num2)
 
-//eventListeners
-acButton.addEventListener("click", ()=>{
+    } else {console.log(`Error occurred after button was clicked!`)}
+}
+function two() {
+    if (operator == ``) {
+        if(num1 == `0`) {
+            num1 = `2`
+            displayUpdater(num1)
+        } else {
+            num1 += `2`
+            displayUpdater(num1)
+        }
+    } else if (operator != ``) {
+        num2 += `2`
+        displayUpdater(num2)
+
+    } else {console.log(`Error occurred after button was clicked!`)}
+}
+function three() {
+    if (operator == ``) {
+        if(num1 == `0`) {
+            num1 = `3`
+            displayUpdater(num1)
+        } else {
+            num1 += `3`
+            displayUpdater(num1)
+        }
+    } else if (operator != ``) {
+        num2 += `3`
+        displayUpdater(num2)
+
+    } else {console.log(`Error occurred after button was clicked!`)}
+}
+function four() {
+    if (operator == ``) {
+        if(num1 == `0`) {
+            num1 = `4`
+            displayUpdater(num1)
+        } else {
+            num1 += `4`
+            displayUpdater(num1)
+        }
+    } else if (operator != ``) {
+        num2 += `4`
+        displayUpdater(num2)
+
+    } else {console.log(`Error occurred after button was clicked!`)}
+}
+function five() {
+    if (operator == ``) {
+        if(num1 == `0`) {
+            num1 = `5`
+            displayUpdater(num1)
+        } else {
+            num1 += `5`
+            displayUpdater(num1)
+        }
+    } else if (operator != ``) {
+        num2 += `5`
+        displayUpdater(num2)
+
+    } else {console.log(`Error occurred after button was clicked!`)}
+}
+function six() {
+    if (operator == ``) {
+        if(num1 == `0`) {
+            num1 = `6`
+            displayUpdater(num1)
+        } else {
+            num1 += `6`
+            displayUpdater(num1)
+        }
+    } else if (operator != ``) {
+        num2 += `6`
+        displayUpdater(num2)
+
+    } else {console.log(`Error occurred after button was clicked!`)}
+}
+function seven() {
+    if (operator == ``) {
+        if(num1 == `0`) {
+            num1 = `7`
+            displayUpdater(num1)
+        } else {
+            num1 += `7`
+            displayUpdater(num1)
+        }
+    } else if (operator != ``) {
+        if(num2 == `0`) {
+            num2 = `7`
+            displayUpdater(num1)
+        } else {
+            num2 += `7`
+            displayUpdater(num1)
+        }
+    } else {console.log(`Error occurred after button was clicked!`)}
+}
+function eight() {
+    if (operator == ``) {
+        if(num1 == `0`) {
+            num1 = `8`
+            displayUpdater(num1)
+        } else {
+            num1 += `8`
+            displayUpdater(num1)
+        }
+    } else if (operator != ``) {
+        if(num2 == `0`) {
+            num2 = `8`
+            displayUpdater(num1)
+        } else {
+            num2 += `8`
+            displayUpdater(num1)
+        }
+    } else {console.log(`Error occurred after button was clicked!`)}
+}
+function nine() {
+    if (operator == ``) {
+        if(num1 == `0`) {
+            num1 = `9`
+            displayUpdater(num1)
+        } else {
+            num1 += `9`
+            displayUpdater(num1)
+        }
+    } else if (operator != ``) {
+        num2 += `9`
+        displayUpdater(num2)
+
+    } else {console.log(`Error occurred after button was clicked!`)}
+}
+function zero() {
+    if (operator == ``) {
+        num1 += `0`
+        displayUpdater(num1)
+    } else if (operator != ``) {
+        num2 += `0`
+        displayUpdater(num2)
+
+    } else {console.log(`Error occurred after button was clicked!`)}
+}
+
+
+function addOperator() {
+    operate()        
+    
+    operator = `+`
+    actualAddButton.style.backgroundColor = `#000e3d`
+    actualDivideButton.style.backgroundColor = ``
+    actualMultiplyButton.style.backgroundColor = ``
+    actualSubtractButton.style.backgroundColor = ``
+
+    decimalButton.addEventListener("click", decimalAdder)
+
+    displayUpdater(operator)
+}
+function subtractOperator() {
+    operate()        
+    
+    operator = `-`
+    actualSubtractButton.style.backgroundColor = `#000e3d`
+    actualDivideButton.style.backgroundColor = ``
+    actualMultiplyButton.style.backgroundColor = ``
+    actualAddButton.style.backgroundColor = ``
+    
+    decimalButton.addEventListener("click", decimalAdder)
+
+    displayUpdater(operator)
+}
+function multiplyOperator() {
+    operate()        
+    
+    operator = `*`
+    actualMultiplyButton.style.backgroundColor = `#000e3d`
+    actualDivideButton.style.backgroundColor = ``
+    actualSubtractButton.style.backgroundColor = ``
+    actualAddButton.style.backgroundColor = ``
+
+    decimalButton.addEventListener("click", decimalAdder)
+
+    displayUpdater(operator)
+}
+function divideOperator() {
+    operate()
+        
+    operator = `/`
+    actualDivideButton.style.backgroundColor = `#000e3d`
+    actualMultiplyButton.style.backgroundColor = ``
+    actualSubtractButton.style.backgroundColor = ``
+    actualAddButton.style.backgroundColor = ``
+
+    decimalButton.addEventListener("click", decimalAdder)
+    
+    displayUpdater(operator)
+}
+
+function AC() {
     displayUpdater(``,``,true)
     num1 = `0`
     num2 = ``
@@ -132,9 +343,11 @@ acButton.addEventListener("click", ()=>{
     actualSubtractButton.style.backgroundColor = ``
 
     decimalButton.addEventListener("click", decimalAdder)
-})
-perCentButton.addEventListener("click", ()=>{num1 = num1/100; displayUpdater(num1)})
-equalButton.addEventListener("click", ()=>{
+}
+
+function perCent() {num1 = num1/100; displayUpdater(num1)}
+
+function equal() {
     operate()
 
 
@@ -144,8 +357,9 @@ equalButton.addEventListener("click", ()=>{
     actualDivideButton.style.backgroundColor = ``
     actualMultiplyButton.style.backgroundColor = ``
     actualSubtractButton.style.backgroundColor = ``
-})
-backspaceButton.addEventListener(`click`, () => {
+}
+
+function backspace() {
     if (operator == ``) {
         num1 = num1.slice(0, -1)
         displayUpdater(num1)
@@ -154,110 +368,40 @@ backspaceButton.addEventListener(`click`, () => {
         displayUpdater(num2)
 
     } else {console.log(`Error occurred after button was clicked!`)}
-})
+}
+
+//eventListeners
+acButton.addEventListener("click", AC)
+perCentButton.addEventListener("click", perCent)
+equalButton.addEventListener("click", equal)
+backspaceButton.addEventListener(`click`, backspace)
 
 
 
 
-sevenButton.addEventListener("click", ()=>{
-        if (operator == ``) {
-            if(num1 == `0`) {
-                num1 = `7`
-                displayUpdater(num1)
-            } else {
-                num1 += `7`
-                displayUpdater(num1)
-            }
-        } else if (operator != ``) {
-            if(num2 == `0`) {
-                num2 = `7`
-                displayUpdater(num1)
-            } else {
-                num2 += `7`
-                displayUpdater(num1)
-            }
-        } else {console.log(`Error occurred after button was clicked!`)}
-})
-eightButton.addEventListener("click", ()=>{
-        if (operator == ``) {
-            if(num1 == `0`) {
-                num1 = `8`
-                displayUpdater(num1)
-            } else {
-                num1 += `8`
-                displayUpdater(num1)
-            }
-        } else if (operator != ``) {
-            if(num2 == `0`) {
-                num2 = `7`
-                displayUpdater(num1)
-            } else {
-                num2 += `7`
-                displayUpdater(num1)
-            }
-        } else {console.log(`Error occurred after button was clicked!`)}
-})
-nineButton.addEventListener("click", ()=>{
-            if (operator == ``) {
-                if(num1 == `0`) {
-                    num1 = `9`
-                    displayUpdater(num1)
-                } else {
-                    num1 += `9`
-                    displayUpdater(num1)
-                }
-            } else if (operator != ``) {
-                num2 += `9`
-                displayUpdater(num2)
-    
-            } else {console.log(`Error occurred after button was clicked!`)}
-})
-fourButton.addEventListener("click", ()=>{
-        if (operator == ``) {
-            if(num1 == `0`) {
-                num1 = `4`
-                displayUpdater(num1)
-            } else {
-                num1 += `4`
-                displayUpdater(num1)
-            }
-        } else if (operator != ``) {
-            num2 += `4`
-            displayUpdater(num2)
+sevenButton.addEventListener("click", seven)
+eightButton.addEventListener("click", eight)
+nineButton.addEventListener("click", nine)
+fourButton.addEventListener("click", four)
+fiveButton.addEventListener("click", five)
+sixButton.addEventListener("click", six)
+oneButton.addEventListener("click", one)
+twoButton.addEventListener("click", two)
+threeButton.addEventListener("click", three)
+zeroButton.addEventListener("click", zero)
+decimalButton.addEventListener("click", decimalAdder)
+        
 
-        } else {console.log(`Error occurred after button was clicked!`)}
-})
-fiveButton.addEventListener("click", ()=>{
-        if (operator == ``) {
-            if(num1 == `0`) {
-                num1 = `5`
-                displayUpdater(num1)
-            } else {
-                num1 += `5`
-                displayUpdater(num1)
-            }
-        } else if (operator != ``) {
-            num2 += `5`
-            displayUpdater(num2)
 
-        } else {console.log(`Error occurred after button was clicked!`)}
-})
-sixButton.addEventListener("click", ()=>{
-        if (operator == ``) {
-            if(num1 == `0`) {
-                num1 = `6`
-                displayUpdater(num1)
-            } else {
-                num1 += `6`
-                displayUpdater(num1)
-            }
-        } else if (operator != ``) {
-            num2 += `6`
-            displayUpdater(num2)
+divideButton.addEventListener("click", divideOperator)
+multiplyButton.addEventListener("click", multiplyOperator)
+subtractButton.addEventListener("click", subtractOperator)
+addButton.addEventListener("click", addOperator)
 
-        } else {console.log(`Error occurred after button was clicked!`)}
-})
-oneButton.addEventListener("click", ()=>{
+//Keyboard acces
+document.addEventListener(`keydown`, (e) => {
+    key = e.key
+    if (key == `1`) {
         if (operator == ``) {
             if(num1 == `0`) {
                 num1 = `1`
@@ -271,100 +415,5 @@ oneButton.addEventListener("click", ()=>{
             displayUpdater(num2)
 
         } else {console.log(`Error occurred after button was clicked!`)}
-})
-twoButton.addEventListener("click", ()=>{
-        if (operator == ``) {
-            if(num1 == `0`) {
-                num1 = `2`
-                displayUpdater(num1)
-            } else {
-                num1 += `2`
-                displayUpdater(num1)
-            }
-        } else if (operator != ``) {
-            num2 += `2`
-            displayUpdater(num2)
-
-        } else {console.log(`Error occurred after button was clicked!`)}
-})
-threeButton.addEventListener("click", ()=>{
-        if (operator == ``) {
-            if(num1 == `0`) {
-                num1 = `3`
-                displayUpdater(num1)
-            } else {
-                num1 += `3`
-                displayUpdater(num1)
-            }
-        } else if (operator != ``) {
-            num2 += `3`
-            displayUpdater(num2)
-
-        } else {console.log(`Error occurred after button was clicked!`)}
-})
-zeroButton.addEventListener("click", ()=>{
-        if (operator == ``) {
-            num1 += `0`
-            displayUpdater(num1)
-        } else if (operator != ``) {
-            num2 += `0`
-            displayUpdater(num2)
-
-        } else {console.log(`Error occurred after button was clicked!`)}
-})
-decimalButton.addEventListener("click", decimalAdder)
-        
-
-
-divideButton.addEventListener("click", ()=>{
-        operate()
-        
-        operator = `/`
-        actualDivideButton.style.backgroundColor = `#000e3d`
-        actualMultiplyButton.style.backgroundColor = ``
-        actualSubtractButton.style.backgroundColor = ``
-        actualAddButton.style.backgroundColor = ``
-
-        decimalButton.addEventListener("click", decimalAdder)
-        
-        displayUpdater(operator)
-})
-multiplyButton.addEventListener("click", ()=>{
-        operate()        
-    
-        operator = `*`
-        actualMultiplyButton.style.backgroundColor = `#000e3d`
-        actualDivideButton.style.backgroundColor = ``
-        actualSubtractButton.style.backgroundColor = ``
-        actualAddButton.style.backgroundColor = ``
-
-        decimalButton.addEventListener("click", decimalAdder)
-
-        displayUpdater(operator)
-})
-subtractButton.addEventListener("click", ()=>{
-        operate()        
-    
-        operator = `-`
-        actualSubtractButton.style.backgroundColor = `#000e3d`
-        actualDivideButton.style.backgroundColor = ``
-        actualMultiplyButton.style.backgroundColor = ``
-        actualAddButton.style.backgroundColor = ``
-        
-        decimalButton.addEventListener("click", decimalAdder)
-
-        displayUpdater(operator)
-})
-addButton.addEventListener("click", ()=>{
-        operate()        
-    
-        operator = `+`
-        actualAddButton.style.backgroundColor = `#000e3d`
-        actualDivideButton.style.backgroundColor = ``
-        actualMultiplyButton.style.backgroundColor = ``
-        actualSubtractButton.style.backgroundColor = ``
-
-        decimalButton.addEventListener("click", decimalAdder)
-
-        displayUpdater(operator)
+    }
 })
